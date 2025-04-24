f56018e11626d8ebee51cfc7b39d3250ae349301
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-04-24 23:12:00 +0300

    0.1.1.01

---
+++ src/components/Allodium/Hero.jsx
@@ -0,0 +1,22 @@
+
+const Hero = () => {
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+      <div id="video-frame"
+        className="relative z-10 h-dh w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
+        <div>
+          <div className="mask-clip-path">
+
+          </div>
+        </div>
+
+        <h1 className="text-5xl text-orange-500 font-bold">
+        Welcome to Allodium™
+        </h1>
+      </div>
+    </div>
+  )
+};
+
+export default Hero;
