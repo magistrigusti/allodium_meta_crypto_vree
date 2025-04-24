d087d13287fe0c0d94d70a48f502fd8fc0d6ca26
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-04-24 10:41:28 +0300

    0.1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(), react()
+  ],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        zentry: ['zentry', 'sanf-serif'],
+        general: ['general', 'sanf-serif'],
+        'circular-web': ['circular-web', 'sanf-serif'],
+        'robert-medium': ['robert-medium', 'sanf-serif'],
+        'robert-regular': ['robert-regular', 'sanf-serif'],
+      },
+      colors: {
+        blue: {
+          50: '#DFDFF0',
+          75: '#DFDFF2',
+          100: '#F0F2FA',
+          200: '#010101',
+          300: '#4FB7DD',
+        },
+        violet: {
+          300: '#5724FF',
+        },
+        yellow: {
+          100: '#8E983F',
+          300: '#EDFF66',
+        }
+      }
+    },
+  },
 })
